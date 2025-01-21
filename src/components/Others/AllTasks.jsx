7dc6f750a1062782b6e2a07436890935979bccb4
Abhi2609 <tiwariabhijeet2609@gmail.com>
2025-01-21 18:57:05 +0530

    completed admin dashboard

---
+++ src/components/Others/AllTasks.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const AllTasks = () => {
+  return (
+    <div id='alltasks' className='mt-4 bg-[#2c2c2c] p-5 rounded h-40 overflow-y-auto'>
+        <div className='bg-red-400 py-2 px-4 mb-2 flex justify-between rounded'>
+            <h2>Abhijeet</h2>
+            <h3>Make a UI design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-green-400 py-2 px-4 mb-2 flex justify-between rounded'>
+            <h2>Abhijeet</h2>
+            <h3>Make a UI design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-blue-400 py-2 px-4 mb-2 flex justify-between rounded'>
+            <h2>Abhijeet</h2>
+            <h3>Make a UI design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-yellow-400 py-2 px-4 mb-2 flex justify-between rounded'>
+            <h2>Abhijeet</h2>
+            <h3>Make a UI design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-red-400 py-2 px-4 mb-2 flex justify-between rounded'>
+            <h2>Abhijeet</h2>
+            <h3>Make a UI design</h3>
+            <h5>Status</h5>
+        </div>
+    </div>
+  )
+}
+
+export default AllTasks
